38b937e236a629ad154104b4f331f0b161e9dbfa
JoannaChang <joannahanchang@gmail.com>
2020-09-27 14:30:01 -0700

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input} from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -61,9 +61,20 @@ function RenderCampsite(props) {
                         
             } else if (recognizeComment(gestureState)) {
                 props.onShowModal();
-            };
+            }
+            return true;
         }
     });  
+
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        }, {
+            dialogTitle: 'Share ' + title
+        });
+    };
 
     if (campsite) {
         return (
@@ -96,6 +107,15 @@ function RenderCampsite(props) {
                         raised
                         reverse
                         onPress={() => props.onShowModal()}
+                    />
+                    <Icon
+                        name={'share'}
+                        type='font-awesome'
+                        color='#5637DD'
+                        style={styles.cardItem}
+                        raised
+                        reverse
+                        onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
                     />
                     </View>
                 </Card>
